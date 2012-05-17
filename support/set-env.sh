4c6dc42308f873c4ec04eb87359011423d4808bb
Ronald Ip <myself@iphoting.com>
2012-05-17 11:22:08 +0800

    Added EC2 launching support scripts.

--- support/set-env.sh
+++ support/set-env.sh
@@ -3,3 +3,7 @@
 export NGINX_VERSION="1.2.0"
 export PHP_VERSION="5.4.1"
 export LIBMCRYPT_VERSION="2.5.8"
+
+export EC2_PRIVATE_KEY=~/.ec2/pk.pem
+export EC2_CERT=~/.ec2/cert.pem
+export EC2_URL=https://ec2.us-east-1.amazonaws.com
