aed08e1957b57948d0bea2a7cb90baae285efdc1
Ronald Ip <myself@iphoting.com>
2012-09-18 10:52:57 +0000

    PHP version: 5.4.7.

--- support/ec2-build-php.sh
+++ support/ec2-build-php.sh
@@ -5,7 +5,7 @@
 ## EDIT
 export S3_BUCKET="heroku-buildpack-php-tyler"
 export LIBMCRYPT_VERSION="2.5.8"
-export PHP_VERSION="5.4.6"
+export PHP_VERSION="5.4.7"
 export APC_VERSION="3.1.10"
 export PHPREDIS_VERSION="2.2.1"
 export LIBMEMCACHED_VERSION="1.0.7"
