3497e1b49115a61a7906ed63a9a076b9d2be5667
David Zuelke <dzuelke@gmail.com>
2014-04-03 17:19:59 +0200

    nginx

---
+++ conf/nginx/heroku.conf.php
@@ -0,0 +1,27 @@
+daemon off;
+
+http {
+    server {
+        listen <?=getenv('PORT')?:'8080'?>;
+        
+        root <?=getenv('DOCUMENT_ROOT')?:getenv('DIR')?:getcwd()?>;
+        
+        location ~ \.php {
+            try_files $uri 404;
+            
+            include fastcgi_params;
+            fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            fastcgi_pass 127.0.0.1:4999;
+            fastcgi_buffers 256 4k;
+        }
+        
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   html;
+        }
+        
+        location / {
+            index  index.php index.html index.htm;
+        }
+    }
+}
