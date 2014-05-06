26c16dee1b81dfcc313702a136088deb7bc4230e
David Zuelke <dzuelke@gmail.com>
2014-05-05 19:29:18 -0700

    refactor nginx configs, add a few default access rules, cleanup, comment

---
+++ conf/nginx/default_include.conf.php
@@ -0,0 +1,8 @@
+location / {
+    index  index.php index.html index.htm;
+}
+
+# for people with app root as doc root, restrict access to a few things
+location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
+    deny all;
+}
--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -18,6 +18,7 @@ http {
 
     fastcgi_buffers 256 4k;
 
+    # define an easy to reference name that can be used in fastgi_pass
     upstream heroku-fcgi {
         #server 127.0.0.1:4999 max_fails=3 fail_timeout=3s;
         server unix:/tmp/heroku.fcgi.<?=getenv('PORT')?:'8080'?>.sock max_fails=3 fail_timeout=3s;
@@ -34,11 +35,26 @@ http {
         
         include <?=getenv('HEROKU_PHP_NGINX_CONFIG_INCLUDE')?>;
         
+        # restrict access to hidden files, just in case
+        location ~ /\. {
+            deny all;
+        }
+        
         # default handling of .php
         location ~ \.php {
-            try_files $uri =404;
             include fastcgi_params;
-            fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            
+            fastcgi_split_path_info ^(.+\.php)(/.*)$;
+            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            # try_files resets $fastcgi_path_info, see http://trac.nginx.org/nginx/ticket/321, so we use the if instead
+            fastcgi_param PATH_INFO $fastcgi_path_info;
+            
+            if (!-f $document_root$fastcgi_script_name) {
+                # check if the script exists
+                # otherwise, /foo.jpg/bar.php would get passed to FPM, which wouldn't run it as it's not in the list of allowed extensions, but this check is a good idea anyway, just in case
+                return 404;
+            }
+            
             fastcgi_pass heroku-fcgi;
         }
     }
