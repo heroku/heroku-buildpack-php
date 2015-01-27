de13c01b7a620148213fad2e3170d6521ee3026f
David Zuelke <dzuelke@gmail.com>
2015-01-27 12:53:58 -0800

    move HHVM FastCGI options to INI

--- conf/hhvm/php.ini.php
+++ conf/hhvm/php.ini.php
@@ -6,3 +6,7 @@ hhvm.log.level = Warning
 hhvm.log.always_log_unhandled_exceptions = true
 hhvm.log.runtime_error_reporting_level = 8191
 hhvm.mysql.typed_results = false
+
+; hhvm fcgi
+hhvm.server.type = fastcgi
+hhvm.server.file_socket = /tmp/heroku.fcgi.<?=getenv('PORT')?>.sock
