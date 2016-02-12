f0b8b83dfdee923d9539e563ced55a376523bc5f
David Zuelke <dzuelke@gmail.com>
2016-02-12 01:08:01 +0100

    streamline passing of data to manifest.py

--- support/build/_util/include/manifest.py
+++ support/build/_util/include/manifest.py
@@ -1,27 +1,22 @@
 import os, sys, json, re, datetime
 
-require = {
-    "heroku-sys/"+os.getenv("STACK"): "^1.0.0",
-    "heroku/installer-plugin": "^1.0.0",
-}
-engine=re.match('heroku-sys-(\w+)-extension', sys.argv[1])
-if engine:
-    require["heroku-sys/"+engine.group(1)] = sys.argv.pop(5)
+require = json.loads(sys.argv[5])
+require["heroku-sys/"+os.getenv("STACK")] = "^1.0.0"
+require["heroku/installer-plugin"] = "^1.0.0"
 
 manifest = {
     "type": sys.argv[1],
     "name": sys.argv[2],
     "version": sys.argv[3],
-    "require": require,
-    "conflict": dict(item.split(":") for item in sys.argv[5:]),
     "dist": {
         "type": "heroku-sys-tar",
         "url": "https://"+os.getenv("S3_BUCKET")+"."+os.getenv("S3_REGION", "s3")+".amazonaws.com/"+os.getenv("S3_PREFIX")+"/"+sys.argv[4]
     },
+    "require": require,
+    "conflict": json.loads(sys.argv[6]) if len(sys.argv) > 6 else {},
+    "replace": json.loads(sys.argv[7]) if len(sys.argv) > 7 else {},
+    "extra": json.loads(sys.argv[8]) if len(sys.argv) > 8 else {},
     "time": datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 }
-
-if not sys.stdin.isatty():
-    manifest["replace"] = dict(item.rstrip("\n").split(" ") for item in tuple(sys.stdin))
 
 json.dump(manifest, sys.stdout, sort_keys=True)
