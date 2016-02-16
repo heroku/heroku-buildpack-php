65a2ac204c8362cc30f0eeed8aa9111bef5a4339
David Zuelke <dzuelke@gmail.com>
2016-02-16 17:58:21 +0100

    --publish flag for deploy.sh to insta-rebuild repo

--- support/build/_util/deploy.sh
+++ support/build/_util/deploy.sh
@@ -5,8 +5,34 @@ set -o pipefail
 # fail harder
 set -eu
 
+publish=false
+
+# process flags
+optstring=":-:"
+while getopts "$optstring" opt; do
+	case $opt in
+		-)
+			case "$OPTARG" in
+				publish)
+					publish=true
+					break
+					;;
+				*)
+					OPTIND=1
+					break
+					;;
+			esac
+	esac
+done
+# clear processed "publish" argument
+shift $((OPTIND-1))
+
 if [[ $# -lt 1 ]]; then
-	echo "Usage: $(basename $0) [--overwrite] FORMULA-VERSION" >&2
+	echo "Usage: $(basename $0) [--publish] FORMULA-VERSION [--overwrite]" >&2
+	echo "  If --publish is given, mkrepo.sh will be invoked after a successful deploy to" >&2
+	echo "  re-generate the repo. CAUTION: this will cause all manifests in the bucket to" >&2
+	echo "  be included in the repo, including potentially currently unpublished ones." >&2
+	echo " All additional arguments, including --overwrite, are passed through to 'bob'." >&2
 	exit 2
 fi
 
@@ -29,3 +55,8 @@ bob deploy "$@"
 echo ""
 echo "Uploading manifest..."
 . $MANIFEST_CMD
+
+if $publish; then
+	echo "Updating repository..."
+	$(dirname $BASH_SOURCE)/mkrepo.sh --upload "$S3_BUCKET" "/${S3_PREFIX}/"
+fi
