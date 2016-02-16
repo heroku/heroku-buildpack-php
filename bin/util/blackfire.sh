a55e3a95c83155a550d607571fc738e1a107a1a2
David Zuelke <dzuelke@gmail.com>
2016-02-16 03:28:53 +0100

    do not load exts when checking add-on exts to install
    avoids newrelic, blackfire etc starting up and creating files or telemetry or whatever

--- bin/util/blackfire.sh
+++ bin/util/blackfire.sh
@@ -5,8 +5,8 @@ install_blackfire_ext() {
     # otherwise users would have to have it in their require section, which is annoying in development environments
     BLACKFIRE_SERVER_ID=${BLACKFIRE_SERVER_ID:-}
     BLACKFIRE_SERVER_TOKEN=${BLACKFIRE_SERVER_TOKEN:-}
-    if [[ "$engine" == "php" && -n "$BLACKFIRE_SERVER_TOKEN" && -n "$BLACKFIRE_SERVER_ID" ]] && ! $engine $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-blackfire; then
-        if $engine $(which composer) require --quiet --update-no-dev -d "$build_dir/.heroku/php" -- "heroku-sys/ext-blackfire:*"; then
+    if [[ "$engine" == "php" && -n "$BLACKFIRE_SERVER_TOKEN" && -n "$BLACKFIRE_SERVER_ID" ]] && ! $engine -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-blackfire; then
+        if $engine -n $(which composer) require --quiet --update-no-dev -d "$build_dir/.heroku/php" -- "heroku-sys/ext-blackfire:*"; then
             echo "- Blackfire detected, installed ext-blackfire" | indent
         else
             warning_inline "Blackfire detected, but no suitable extension available"
