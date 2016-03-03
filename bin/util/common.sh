9adb7fa443962eb2a344399a1d444346bbd4f7ff
David Zuelke <dzuelke@gmail.com>
2016-03-03 14:49:08 +0100

    do not export IFS var

--- bin/util/common.sh
+++ bin/util/common.sh
@@ -44,7 +44,7 @@ indent() {
 export_env_dir() {
   local env_dir=$1
   local whitelist_regex=${2:-''}
-  local blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+  local blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|IFS)$'}
   if [ -d "$env_dir" ]; then
     for e in $(ls $env_dir); do
       echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
