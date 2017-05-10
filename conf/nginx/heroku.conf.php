8498f884c1037570c16a7962fa22be3917558030
michael-tomala <michal.tomala@lama-media.com>
2017-05-10 21:34:32 +0200

    Update heroku.conf.php

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -7,7 +7,7 @@ http {
     #                  '"$http_user_agent" "$http_x_forwarded_for"';
 
     #access_log  logs/access.log  main;
-
+    
     sendfile        on;
     #tcp_nopush     on;
 
@@ -39,6 +39,8 @@ http {
     }
     
     server {
+        rewrite_log on;
+            
         # define an easy to reference name that can be used in try_files
         location @heroku-fcgi {
             include fastcgi_params;
@@ -71,6 +73,32 @@ http {
         include "<?=getenv('HEROKU_PHP_NGINX_CONFIG_INCLUDE')?>";
         
         index /app/html/index.php;
+            
+        # Global restrictions configuration file.
+        # Designed to be included in any server {} block.
+        location = /favicon.ico {
+            log_not_found off;
+            access_log off;
+        }
+
+        location = /robots.txt {
+            allow all;
+            log_not_found off;
+            access_log off;
+        }
+
+        # Deny all attempts to access hidden files such as .htaccess, .htpasswd, .DS_Store (Mac).
+        # Keep logging the requests to parse later (or to pass to firewall utilities such as fail2ban)
+        location ~ /\. {
+            deny all;
+        }
+
+        # Deny access to any files with a .php extension in the uploads directory
+        # Works in sub-directory installs and also in multisite network
+        # Keep logging the requests to parse later (or to pass to firewall utilities such as fail2ban)
+        location ~* /(?:uploads|files)/.*\.php$ {
+            deny all;
+        }
 
         location ~ ^(/[^/]+/)?files/(.+) {
             try_files /wp-content/blogs.dir/$blogid/files/$2 /wp-includes/ms-files.php?file=$2 ;
@@ -94,9 +122,10 @@ http {
             try_files $uri $uri/ /app/html/index.php?$args ;
         }
 
-        location ~ \.php$ {
-            try_files $uri =404;
-            include fastcgi_params;
+        location ~ \.php$ {            
+            try_files @heroku-fcgi @heroku-fcgi;
+            #try_files $uri =404;
+            #include fastcgi_params;
             #fastcgi_pass php;
         }
 
