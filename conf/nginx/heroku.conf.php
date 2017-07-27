14138e826bb2aba0f7f18990cb00bbe74bf96a04
Marcin Sawicki <marcin.sawicki@lama-media.com>
2017-07-27 15:48:33 +0200

    htpasswd try

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -7,7 +7,7 @@ http {
     #                  '"$http_user_agent" "$http_x_forwarded_for"';
 
     #access_log  logs/access.log  main;
-    
+
     sendfile        on;
     #tcp_nopush     on;
 
@@ -37,43 +37,48 @@ http {
         #Ref: http://wordpress.org/extend/plugins/nginx-helper/
         #include /var/www/wordpress/wp-content/plugins/nginx-helper/map.conf ;
     }
-    
+
     server {
         rewrite_log on;
-            
+
         # define an easy to reference name that can be used in try_files
         location @heroku-fcgi {
             include fastcgi_params;
-            
+
             fastcgi_split_path_info ^(.+\.php)(/.*)$;
             fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
             # try_files resets $fastcgi_path_info, see http://trac.nginx.org/nginx/ticket/321, so we use the if instead
             fastcgi_param PATH_INFO $fastcgi_path_info if_not_empty;
-            
+
             if (!-f $document_root$fastcgi_script_name) {
                 # check if the script exists
                 # otherwise, /foo.jpg/bar.php would get passed to FPM, which wouldn't run it as it's not in the list of allowed extensions, but this check is a good idea anyway, just in case
                 return 404;
             }
-            
+
             fastcgi_pass heroku-fcgi;
         }
-        
+
         # TODO: use X-Forwarded-Host? http://comments.gmane.org/gmane.comp.web.nginx.english/2170
         server_name localhost;
         listen <?=getenv('PORT')?:'8080'?>;
         # FIXME: breaks redirects with foreman
         port_in_redirect off;
-        
+
         root "<?=getenv('DOCUMENT_ROOT')?:getenv('HEROKU_APP_DIR')?:getcwd()?>";
-        
+
         error_log stderr;
         access_log /tmp/heroku.nginx_access.<?=getenv('PORT')?:'8080'?>.log;
-        
+
         include "<?=getenv('HEROKU_PHP_NGINX_CONFIG_INCLUDE')?>";
-        
+
         index index.php;
-        
+
+        <?=if(getenv('PASSWD')==1):?>
+          auth_basic "Restricted";
+          auth_basic_user_file /app/html/.htpasswd;
+        <?=endif?>
+
 		# Global restrictions configuration file.
         # Designed to be included in any server {} block.
         location = /favicon.ico {
@@ -86,13 +91,13 @@ http {
             log_not_found off;
             access_log off;
         }
-		
+
 	# Deny all attempts to access hidden files such as .htaccess, .htpasswd, .DS_Store (Mac).
         # Keep logging the requests to parse later (or to pass to firewall utilities such as fail2ban)
         location ~ /\. {
             deny all;
         }
-	
+
 	# BEGIN W3TC CDN
 	location ~ \.(ttf|ttc|otf|eot|woff|font.css)$ {
 	   add_header Access-Control-Allow-Origin "*";
@@ -142,7 +147,7 @@ http {
 	    rewrite .* "/wp-content/cache/page_enhanced/$http_host/$request_uri/_index$w3tc_ref$w3tc_ssl$w3tc_rewrite$w3tc_ext" last;
 	}
 	# END W3TC Page Cache core
-		
+
 
 	# Deny access to any files with a .php extension in the uploads directory
         # Works in sub-directory installs and also in multisite network
@@ -150,7 +155,7 @@ http {
         location ~* /(?:uploads|files)/.*\.php$ {
             deny all;
         }
-		
+
 		#
 		#
 		location ~ ^(/[^/]+/)?files/(.+) {
@@ -173,13 +178,13 @@ http {
             rewrite ^(/[^/]+)?(/wp-.*) $2 last;
             rewrite ^(/[^/]+)?(/.*\.php) $2 last;
         }
-		
+
 		#
 		#
         location / {
             try_files $uri $uri/ /index.php?$args;
         }
-		
+
 		#
 		#
 		location ~ \.php {
