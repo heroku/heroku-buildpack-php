ac308f69e5e831caa6bcd8821be2f198d144efc5
David Zuelke <dzuelke@gmail.com>
2019-02-27 01:19:09 +0100

    Nginx/1.14.2, fixes #241 and #285

--- test/spec/nginx_spec.rb
+++ test/spec/nginx_spec.rb
@@ -4,9 +4,11 @@ describe "A PHP application" do
 	let(:app) {
 		new_app_with_stack_and_platrepo('test/fixtures/default')
 	}
-	it "installs nginx with OpenSSL support" do
+	it "installs a recent stable nginx with OpenSSL support" do
 		app.deploy do |app|
-			expect(app.output).to match(/- nginx \((\d+\.\d+\.\d+)/)
+			nginx = app.output.match(/- nginx \((\d+\.\d*[02468]\.\d+)/)
+			expect(nginx).not_to be_nil, "expected nginx install line in build output"
+			expect(Gem::Dependency.new('nginx', '~> 1.14').match?('nginx', nginx[1])).to be == true, "expected nginx version compatible with selector '~> 1.14' but got #{nginx[1]}"
 			expect(app.run('nginx -V')).to match(/^built with OpenSSL/)
 		end
 	end
