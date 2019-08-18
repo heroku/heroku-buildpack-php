28c4cc44af43e8b636914fb18bd9cf82ebde4876
David Zuelke <dzuelke@gmail.com>
2019-08-19 00:59:33 +0200

    drop cedar-14 tests

--- test/spec/php_shared_base.rb
+++ test/spec/php_shared_base.rb
@@ -32,7 +32,7 @@ shared_examples "A basic PHP application" do |series|
 			expect(@app.run("php -i | grep memory_limit")).to match "memory_limit => 536870912 => 536870912"
 		end
 		
-		it "is running a PHP build that links against libc-client, libonig, libsqlite3 and libzip from the stack", :if => series.between?("7.2","8.0") && ENV["STACK"] != "cedar-14" do
+		it "is running a PHP build that links against libc-client, libonig, libsqlite3 and libzip from the stack", :if => series.between?("7.2","8.0") do
 			ldd_output = @app.run("ldd .heroku/php/bin/php .heroku/php/lib/php/extensions/no-debug-non-zts-*/{imap,mbstring,pdo_sqlite,sqlite3}.so | grep -E ' => (/usr)?/lib/' | grep -e 'libc-client.so' -e 'libonig.so' -e 'libsqlite3.so' -e 'libzip.so' | wc -l")
 			# 1x libc-client.so for extensions/…/imap.so
 			# 1x libonig for extensions/…/mbstring.so
