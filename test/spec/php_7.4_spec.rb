5796f9a3995c4e823caa4f6c13ee5bbdddd6bdc3
David Zuelke <dzuelke@gmail.com>
2020-01-31 16:34:01 +0100

    FPM slowlog and terminate tests

--- test/spec/php_7.4_spec.rb
+++ test/spec/php_7.4_spec.rb
@@ -26,6 +26,20 @@ describe "A PHP 7.4 application with a composer.json", :requires_php_on_stack =>
 						expect(output).to include("sleep() /app/index.php:5")
 					end
 				end
+				
+				it "logs slowness after about 3 seconds and terminates the process after about 30 seconds" do
+					app.deploy do |app|
+						# first, launch in the background wrapped in a 45 second timeout
+						# then sleep three seconds to allow boot
+						# curl the sleep() script with a very long timeout
+						# ensure slowlog and terminate output is there
+						# wait for timeout process
+						cmd = "timeout 45 heroku-php-apache2 & sleep 3; curl \"localhost:$PORT/index.php?wait=35\"; wait"
+						output = app.run(cmd.shellescape)
+						expect(output).to match(/executing too slow/)
+						expect(output).to match(/execution timed out/)
+					end
+				end
 			end
 		end
 	end
