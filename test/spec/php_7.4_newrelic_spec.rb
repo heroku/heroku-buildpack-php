16ccdddf56e8c146cdc4caf19d61fc6f3be63e15
David Zuelke <dzuelke@salesforce.com>
2020-10-07 19:06:13 +0200

    simplify New Relic tests
    we only need to test the behavior of detection and startup suppression, none of which depends on the PHP version, so we only run it once using the latest, rather than for each PHP series

--- test/spec/php_7.4_newrelic_spec.rb
+++
@@ -1,5 +0,0 @@
-require_relative "php_shared_newrelic"
-
-describe "A PHP 7.4 application with a composer.json", :requires_php_on_stack => "7.4" do
-	include_examples "A PHP application with a composer.json", "7.4"
-end
