4c1a77a1829d716c4a22a7b22b123f2ddf483c6e
David Zuelke <dzuelke@salesforce.com>
2021-08-12 14:50:42 +0200

    Composer Platform Installer Plugin tests
    Invokes a real install using the "base" generator fixture case, then validates that
    - package installs are merged into the same dir structure
    - an `export` script is written
    - `profile.d/` scripts are written, with file names reflecting order of install
    - configs for PHP extensions are written, with file names reflecting order of install
    - shared extensions bundled by PHP are enabled if a project requires them

--- test/spec/platform_spec.rb
+++ test/spec/platform_spec.rb
@@ -2,6 +2,7 @@ require "ansi/core"
 require "json"
 require "open3"
 require "shellwords"
+require "tempfile"
 
 generator_fixtures_subdir = "test/fixtures/platform/generator"
 
@@ -70,6 +71,65 @@ describe "The PHP Platform Installer" do
 					expect(status.exitstatus).to eq(0), "dry run install failed, stderr: #{stderr}, stdout: #{stdout}"
 				end
 			end
+		end
+	end
+	
+	describe "Composer Plugin" do
+		before(:all) do
+			@install_tmpdir = Dir.mktmpdir(nil, generator_fixtures_subdir) # this needs to be on the same level as the source fixture so the relative path references to the installer plugin inside composer.json work
+			@export_tmpfile = Tempfile.new("export")
+			@profiled_tmpdir = Dir.mktmpdir("profile.d")
+			FileUtils.cp("#{generator_fixtures_subdir}/base/expected_platform_composer.json", "#{@install_tmpdir}/composer.json")
+			Dir.chdir(@install_tmpdir) do
+				cmd = "export_file_path=#{@export_tmpfile.path} profile_dir_path=#{@profiled_tmpdir} composer install --no-dev"
+				@stdout, @stderr, @status = Open3.capture3("bash -c #{Shellwords.escape(cmd)}")
+			end
+		end
+		
+		after(:all) do
+			FileUtils.remove_entry(@install_tmpdir)
+			FileUtils.remove_entry(@profiled_tmpdir)
+			@export_tmpfile.unlink
+		end
+		
+		it "performs an installation successfully" do
+			expect(@status.exitstatus).to eq(0), "composer install failed, stderr: #{@stderr}, stdout: #{@stdout}"
+		end
+		
+		it "installs multiple packages into the same directory structure" do
+			Dir.chdir(@install_tmpdir) do
+				expect(File.exist?("bin/php")).to eq(true)
+				expect(File.exist?("bin/composer")).to eq(true)
+				expect(File.exist?("sbin/httpd")).to eq(true)
+				expect(File.exist?("sbin/nginx")).to eq(true)
+			end
+		end
+		
+		it "writes an export script" do
+			expect(@export_tmpfile.size).to be > 0
+		end
+		
+		it "writes profile scripts so they get sourced in the order of package installs" do
+			# fetch all profile.d files and sort them (Bash on startup globs them in alnum sort order, but Ruby's Dir has no guaranteed order)
+			profiledscripts = Dir.each_child(@profiled_tmpdir).sort
+			# remember from installer output if PHP was installed first or Composer
+			order_to_check = @stderr.index("Installing heroku-sys/php ") > @stderr.index("Installing heroku-sys/composer ")
+			# now expect the same order in `.profile.d/` for the two packages' scripts
+			expect(profiledscripts.index {|f| f.include?("php.sh")} > profiledscripts.index {|f| f.include?("php.sh")}).to eq(order_to_check)
+		end
+		
+		it "writes extension configs so they get loaded in the order of package installs" do
+			# fetch all extension INI files and sort them (PHP reads them in alnum sort order on startup, but Ruby's Dir has no guaranteed order)
+			extensionconfigs = Dir.each_child("#{@install_tmpdir}/etc/php/conf.d").sort
+			# remember from installer outout if ext-blackfire was installed first or ext-redis
+			order_to_check = @stderr.index("Installing heroku-sys/ext-blackfire ") > @stderr.index("Installing heroku-sys/ext-redis ")
+			# now expect the same order in `conf.d/` for the two extensions' configs
+			expect(extensionconfigs.index {|f| f.include?("ext-blackfire.ini")} > extensionconfigs.index {|f| f.include?("ext-redis.ini")}).to eq(order_to_check)
+		end
+		
+		it "enables shared extensions bundled with PHP if necessary" do
+			expect(@stderr).to match("Enabling heroku-sys/ext-mbstring")
+			expect(Dir.entries("#{@install_tmpdir}/etc/php/conf.d").any? {|f| f.include?("ext-mbstring.ini")}).to eq(true)
 		end
 	end
 end
