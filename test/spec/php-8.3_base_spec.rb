28a39069657849f5a1353398c181b36e19eceae6
David Zuelke <dzuelke@salesforce.com>
2024-01-23 16:01:42 +0100

    centralize and re-use timeout/sigterm related tests

--- test/spec/php-8.3_base_spec.rb
+++ test/spec/php-8.3_base_spec.rb
@@ -2,35 +2,4 @@ require_relative "php_base_shared"
 
 describe "A basic PHP 8.3 application", :requires_php_on_stack => "8.3" do
 	include_examples "A basic PHP application", "8.3"
-	
-	context "with an index.php that allows for different execution times" do
-		['apache2', 'nginx'].each do |server|
-			context "running the #{server} web server" do
-				let(:app) {
-					new_app_with_stack_and_platrepo('test/fixtures/sigterm',
-						before_deploy: -> { system("composer require --quiet --ignore-platform-reqs php '8.3.*'") or raise "Failed to require PHP version" }
-					)
-				}
-				
-				# FIXME: move to php_shared.rb once all PHPs are rebuilt with that tracing capability
-				it "logs slowness after about 3 seconds, prints a trace, and terminates the process after about 30 seconds" do
-					app.deploy do |app|
-						# launch web server wrapped in a 40 second timeout
-						# once web server is ready, `read` unblocks and we curl the sleep() script which will take a few seconds to run
-						# after `curl` completes, `waitforit.sh` will shut down
-						cmd = "./waitforit.sh 40 'ready for connections' heroku-php-#{server} --verbose | { read && curl \"localhost:$PORT/index.php?wait=35\"; }"
-						retry_until retry: 3, sleep: 5 do
-							output = app.run(cmd)
-							# ensure slowlog info and trace is there
-							expect(output).to include("executing too slow")
-							expect(output).to include("sleep() /app/index.php:5")
-							# ensure termination info is there
-							expect(output).to match(/execution timed out/)
-							expect(output).to match(/exited on signal/)
-						end
-					end
-				end
-			end
-		end
-	end
 end
--- test/spec/php_sigterm_shared.rb
+++ test/spec/php_sigterm_shared.rb
@@ -1,9 +1,12 @@
 require_relative "spec_helper"
 
-describe "A PHP application" do
-	context "with an index.php that allows for different execution times" do
+shared_examples "A PHP application with long-running requests" do |series|
+	context "that uses #{series}" do
 		before(:all) do
-			@app = new_app_with_stack_and_platrepo('test/fixtures/sigterm', run_multi: true)
+			@app = new_app_with_stack_and_platrepo('test/fixtures/sigterm',
+				before_deploy: -> { system("composer require --quiet --ignore-platform-reqs php '#{series}.*'") or raise "Failed to require PHP version" },
+				run_multi: true
+			)
 			@app.deploy
 		end
 		
@@ -26,7 +29,7 @@ describe "A PHP application" do
 					end
 				end
 				
-				it "gracefully shuts down when all processes receives a SIGTERM because HEROKU_PHP_GRACEFUL_SIGTERM is on by default" do
+				it "gracefully shuts down when all processes receive a SIGTERM because HEROKU_PHP_GRACEFUL_SIGTERM is on by default" do
 					# first, launch in the background and get the pid
 					# then sleep five seconds to allow boot (semicolon before ! needs a space, Bash...)
 					# curl the sleep() script (and remember the curl pid)
@@ -38,6 +41,21 @@ describe "A PHP application" do
 						output = @app.run(cmd)
 						expect(output).to match(/^hello world after 5 second\(s\)$/)
 						expect(output).to match(/^request complete$/) # ensure a late log line is captured, meaning the logs tail process stays alive until the end
+					end
+				end
+				
+				it "logs slowness after about 3 seconds, prints a trace, and terminates the process after about 30 seconds" do
+					# launch web server wrapped in a 40 second timeout
+					# once web server is ready, `read` unblocks and we curl the sleep() script which will take a few seconds to run
+					# after `curl` completes, `waitforit.sh` will shut down
+					cmd = "./waitforit.sh 40 'ready for connections' heroku-php-#{server} --verbose | { read && curl \"localhost:$PORT/index.php?wait=35\"; }"
+					retry_until retry: 3, sleep: 5 do
+						output = @app.run(cmd)
+						# ensure slowlog info and trace is there
+						expect(output).to include("executing too slow")
+						expect(output).to include("sleep() /app/index.php:5")
+						# ensure termination info is there
+						expect(output).to match(/execution timed out/)
 					end
 				end
 			end
