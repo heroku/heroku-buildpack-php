3866053e0f89d46eabd406ffbe8443a9f4d48466
David Zuelke <dzuelke@salesforce.com>
2024-01-26 13:51:20 +0100

    verbose mode for autotune.php and RAM detection
    GUS-W-14901894

--- bin/util/autotune.php
+++ bin/util/autotune.php
@@ -29,7 +29,7 @@ function bytestostring($amount) {
 	return sprintf("%d%s", $amount, $suffix);
 }
 
-$opts = getopt("t:", array(), $rest_index);
+$opts = getopt("vt:", array(), $rest_index);
 $argv = array_slice($argv, $rest_index);
 $argc = count($argv);
 if($argc < 1 || $argc > 2) {
@@ -40,42 +40,11 @@ if($argc < 1 || $argc > 2) {
 	);
 	fputs(STDERR,
 		"Options:\n".
+		"  -v                 Verbose mode\n".
 		"  -t <DOCUMENT_ROOT> Dir to read '.user.ini' with 'memory_limit' settings from\n\n".
 		"php_value or php_admin_value lines from a PHP-FPM config can be fed via STDIN.\n\n"
 	);
 	exit(2);
-}
-
-// first, parse potential php_value and php_admin_value data from STDIN
-// the expected format is lines like the following:
-// php_value[memory_limit] = 128M
-// php_admin_value[memory_limit] = 128M
-$limits = (stream_isatty(STDIN) ? [] : parse_ini_string(stream_get_contents(STDIN)));
-if($limits === false) {
-	fputs(STDERR, "ERROR: Malformed FPM php_value/php_admin_value directives on STDIN.\n");
-	exit(1);
-}
-
-if(
-	isset($opts['t']) &&
-	is_readable($opts['t'].'/.user.ini')
-) {
-	// we only read the topmost .user.ini inside document root
-	$userini = parse_ini_file($opts['t'].'/.user.ini');
-	if($userini === false) {
-		fputs(STDERR, "ERROR: Malformed .user.ini in document root.\n");
-		exit(1);
-	}
-	if(isset($userini['memory_limit'])) {
-		// if .user.ini has a limit set, it will overwrite an FPM config php_value, but not a php_admin_value
-		$limits['php_value']['memory_limit'] = $userini['memory_limit'];
-	}
-}
-
-if(isset($limits['php_admin_value']['memory_limit'])) {
-	ini_set('memory_limit', $limits['php_admin_value']['memory_limit']);
-} elseif(isset($limits['php_value']['memory_limit'])) {
-	ini_set('memory_limit', $limits['php_value']['memory_limit']);
 }
 
 $ram = stringtobytes($argv[0]); // first arg is the available memory
@@ -90,6 +59,51 @@ if(isset($argv[1])) { // optional second arg is the maximum RAM we're allowed
 		$ram = $max_ram;
 		fprintf(STDERR, "Limiting RAM usage to %s Bytes\n", bytestostring($ram));
 	}
+}
+
+// parse potential php_value and php_admin_value data from STDIN
+// the expected format is lines like the following:
+// php_value[memory_limit] = 128M
+// php_admin_value[memory_limit] = 128M
+$limits = (stream_isatty(STDIN) ? [] : parse_ini_string(stream_get_contents(STDIN)));
+if($limits === false) {
+	fputs(STDERR, "ERROR: Malformed FPM php_value/php_admin_value directives on STDIN.\n");
+	exit(1);
+}
+
+if(isset($opts['v'])) {
+	if(isset($limits['php_value'])) {
+		fputs(STDERR, "memory_limit changed by php_value in PHP-FPM configuration\n");
+	}
+}
+
+if(
+	isset($opts['t']) &&
+	is_readable($userini_path = $opts['t'].'/.user.ini')
+) {
+	// we only read the topmost .user.ini inside document root
+	$userini = parse_ini_file($userini_path);
+	if($userini === false) {
+		fprintf(STDERR, "ERROR: Malformed %s.\n", $userini_path);
+		exit(1);
+	}
+	if(isset($userini['memory_limit'])) {
+		if(isset($opts['v'])) {
+			fprintf(STDERR, "memory_limit changed by %s\n", $userini_path);
+		}
+		// if .user.ini has a limit set, it will overwrite an FPM config php_value, but not a php_admin_value
+		$limits['php_value']['memory_limit'] = $userini['memory_limit'];
+	}
+}
+
+if(isset($limits['php_admin_value']['memory_limit'])) {
+	// these take precedence and cannot be overridden later
+	if(isset($opts['v'])) {
+		fputs(STDERR, "memory_limit overridden by php_admin_value in PHP-FPM configuration\n");
+	}
+	ini_set('memory_limit', $limits['php_admin_value']['memory_limit']);
+} elseif(isset($limits['php_value']['memory_limit'])) {
+	ini_set('memory_limit', $limits['php_value']['memory_limit']);
 }
 
 $limit = ini_get('memory_limit');
