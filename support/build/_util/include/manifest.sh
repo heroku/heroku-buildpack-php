14aa198b339429c5f3e5726f82a97f771c5af4a7
David Zuelke <dzuelke@salesforce.com>
2024-05-20 23:33:51 -0400

    quoting fixes for manifest.sh

--- support/build/_util/include/manifest.sh
+++ support/build/_util/include/manifest.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 print_or_export_manifest_cmd() {
 	if [[ "${MANIFEST_CMD:-}" ]]; then
-		echo "$1" > $MANIFEST_CMD
+		echo "$1" > "$MANIFEST_CMD"
 	else
-		echo "-----> Done. Run '$1' to upload manifest."
+		echo "-----> Done. To upload manifest, run: $1"
 	fi
 }
 
@@ -13,7 +13,7 @@ generate_manifest_cmd() {
 }
 
 soname_version() {
-	soname=$(objdump -p $1 | grep SONAME | awk '{ printf $2; }')
-	file=$(basename $1)
+	soname=$(objdump -p "$1" | grep SONAME | awk '{ printf $2; }')
+	file=$(basename "$1")
 	echo "${soname#${file}.}"
 }
