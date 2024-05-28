b4e5f8befbabede40e057a35a5079a4b4d7e06fc
David Zuelke <dzuelke@salesforce.com>
2024-05-28 12:23:21 +0200

    Test fixtures for cgroups v1/v2 handling
    Covers Docker in various permutations, as well as Heroku's past, present, and possible future behavior on CR and PS.

--- test/fixtures/cgroups/gen-docker-cases.sh
+++ test/fixtures/cgroups/gen-docker-cases.sh
@@ -19,6 +19,7 @@ echo >&2 "You can switch by changing 'deprecatedCgroupv1' in '~/Library/Group Co
 echo >&2 "Remember to quit and start Docker Desktop afterwards (just a restart is not enough)"
 
 img=heroku/heroku:24
+# "1024M" and "800M" are grepped further down, be careful with changes
 cases=(
 	"test/fixtures/cgroups/docker-${cgroup_driver}-v${cgroup_version}-host-noparent-res-limit -m 1024M --memory-reservation 800M --cgroupns host"
 	"test/fixtures/cgroups/docker-${cgroup_driver}-v${cgroup_version}-private-noparent-res-limit -m 1024M --memory-reservation 800M --cgroupns private"
@@ -51,6 +52,10 @@ for case in "${cases[@]}"; do
 	case=${parts[0]} # first part is the case name/dir - caution: array indexing differs if you run this in zsh (starts at 1)
 	opts=${parts[@]:1} # remaining parts are docker opts - caution: array indexing differs if you run this in zsh (starts at 1)
 	echo "Populating ${case}..."
+	# write these out so the test logic does not have to pull massive stunts in order to figure out which of the /sys/fs/cgroup subdirs has the expected value
+	# --memory-reservation sets memory.low, so if there is a -m (memory.max) as well, that takes precedence
+	echo "$opts" | grep -q -- "--memory-reservation 800M" && echo $(( 800 * 1024 * 1024 )) > "$case/expected_stdout"
+	echo "$opts" | grep -q -- "-m 1024M" && echo $(( 1024 * 1024 * 1024 )) > "$case/expected_stdout"
 	mkdir -p "$case/proc/self"
 	mkdir -p "$case/sys/fs/cgroup"
 	# no quotes for $opts is correct
