a37c1b60094844757404290f528858fd8c1b1d37
David Zuelke <dzuelke@salesforce.com>
2025-05-06 14:32:57 +0200

    Handling of EOL Composer 1 lock files (with tests)
    Composer 1 (EOL) is no longer available on heroku-22+, but we still want to test that "platform.json" is generated correctly for lock files that were generated using Composer 1, and that the install fails as expected (also for the case of super old lock files that do not have a plugin-api-version field).

--- bin/util/platform.php
+++ bin/util/platform.php
@@ -218,8 +218,8 @@ $require["heroku-sys/composer"] = "*";
 // otherwise, builds would fail the moment e.g. 2.6.0 comes out and people try it, even though 2.5 could install the project just fine
 $pav = $lock["plugin-api-version"] ?? false;
 if($pav === false) {
-	file_put_contents("php://stderr", "\033[1;33mNOTICE:\033[0m No Composer plugin-api-version recorded in $COMPOSER_LOCK; file must be very old. Will attempt to use Composer 1 for installation.\n");
-	$pav = "1.0.0";
+	file_put_contents("php://stderr", "ERROR: No Composer plugin-api-version recorded in $COMPOSER_LOCK; file must be very old. Please update it using a recent version of Composer.\n");
+	exit(1);
 }
 if(in_array($pav, ["2.0.0", "2.1.0", "2.2.0"])) {
 	// no rule without an exception, of course:
