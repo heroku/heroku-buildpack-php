f460cbfe414c0682a2d1fccd8c1d9ccd96010ac5
David Zuelke <dzuelke@salesforce.com>
2025-06-25 14:36:11 +0200

    WIP: isolate bootstrapped composer

--- bin/util/blackfire.sh
+++ bin/util/blackfire.sh
@@ -5,8 +5,8 @@ install_blackfire_ext() {
 	# otherwise users would have to have it in their require section, which is annoying in development environments
 	BLACKFIRE_SERVER_ID=${BLACKFIRE_SERVER_ID:-}
 	BLACKFIRE_SERVER_TOKEN=${BLACKFIRE_SERVER_TOKEN:-}
-	if [[ -n "$BLACKFIRE_SERVER_TOKEN" && -n "$BLACKFIRE_SERVER_ID" ]] && ! php -n $(which composer2) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-blackfire 2>/dev/null; then
-		if php -n $(which composer2) require --update-no-dev -d "$build_dir/.heroku/php" -- "heroku-sys/ext-blackfire:*" >> $build_dir/.heroku/php/install.log 2>&1; then
+	if [[ -n "$BLACKFIRE_SERVER_TOKEN" && -n "$BLACKFIRE_SERVER_ID" ]] && ! platform-composer show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-blackfire 2>/dev/null; then
+		if platform-composer require --update-no-dev -d "$build_dir/.heroku/php" -- "heroku-sys/ext-blackfire:*" >> $build_dir/.heroku/php/install.log 2>&1; then
 			echo "- Blackfire detected, installed ext-blackfire" | indent
 		else
 			mcount "warnings.addons.blackfire.extension_missing"
