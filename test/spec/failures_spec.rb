cc0c04f5543d60b2c6002b348b3d81e3b1d0d9bd
David Zuelke <dzuelke@salesforce.com>
2025-10-03 16:03:19 +0200

    Tests for closing open timers on exit

---
+++ test/spec/failures_spec.rb
@@ -0,0 +1,44 @@
+require_relative "spec_helper"
+
+describe "A PHP application failing to build" do
+	context "because the composer compile script exits non-zero" do
+		before(:all) do
+			@app = new_app_with_stack_and_platrepo_and_bin_report_dumper(
+				"test/fixtures/composer/compile_script",
+				config: {
+					"COMPILE_SCRIPT_SLEEP" => 1,
+					"COMPILE_SCRIPT_EXIT" => 1,
+				},
+				allow_failure: true,
+			)
+			@app.deploy
+		end
+		
+		after(:all) do
+			@app.teardown!
+		end
+		
+		it "throws an error" do
+			expect(@app.output).to include("Compile step failed!")
+		end
+		
+		it "captures information about the build" do
+			expect(@app.bin_report_dump).to match(
+				"bootstrap.duration" => a_kind_of(Float),
+				"platform.prepare.duration" => a_kind_of(Float),
+				"platform.install.main.packages.installed_count" => 4,
+				"platform.install.main.duration" => a_kind_of(Float),
+				"platform.polyfill_count" => 0,
+				"platform.packages.installed_count" => 4,
+				"platform.php.version" => a_string_matching(/^\d+\.\d+\.\d+$/),
+				"platform.php.series" => a_string_matching(/^\d+\.\d+$/),
+				"platform.install.duration" => a_kind_of(Float),
+				"dependencies.install.duration" => a_kind_of(Float),
+				"dependencies.packages.installed_count" => 0,
+				"open_timers" => "__main__,scripts.compile",
+				"duration" => a_kind_of(Float),
+				"scripts.compile.duration" => a_kind_of(Float).and(a_value > 1),
+			)
+		end
+	end
+end
