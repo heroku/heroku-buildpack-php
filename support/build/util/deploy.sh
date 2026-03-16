04a25979dc656fa87a549e5b591752ed9884685c
David Zuelke <dzuelke@salesforce.com>
2026-03-16 15:45:40 +0100

    Do not fail builds of packages that are not supported on the given stack or architecture
    Some packages, e.g. librdkafka 1 and 2, are only available on certain stacks. Most notably, stacks only support the PHP versions that were in active maintenance mode when the stack was released.
    
    To make it easier to mass-build packages, if a package recipe is now invoked in an environment (currently it's only the stack, but could also be the architecture) that does not support it, the build does not count as failed, and the repo is generated correctly at the end, as it should be.
    
    The necessary handling for the exit status from 'bob deploy' also already handles the case where a package already exists, to allow easy implementation of an option for the build workflow to not fail on existing packages, either.

--- support/build/util/deploy.sh
+++ support/build/util/deploy.sh
@@ -6,7 +6,7 @@ set -o pipefail
 set -eu
 
 help=false
-
+dry_run=false
 publish=false
 
 # process flags
@@ -21,6 +21,10 @@ while getopts "$optstring" opt; do
 				help)
 					help=true
 					;;
+				dry-run)
+					dry_run=true
+					break
+					;;
 				publish)
 					publish=true
 					break
@@ -32,11 +36,13 @@ while getopts "$optstring" opt; do
 			esac
 	esac
 done
-# clear processed "publish" argument
+# clear processed arguments
 shift $((OPTIND-1))
 
 if $help || [[ $# -lt 1 ]]; then
 	cat >&2 <<-EOF
+		Usage: $(basename "$0") --dry-run FORMULA-VERSION
+		  With --dry-run, a 'bob build' will be performed instead of a 'bob deploy'.
 		Usage: $(basename "$0") [--publish] FORMULA-VERSION [--overwrite]
 		  If --publish is given, mkrepo.sh will be invoked after a successful deploy to
 		  re-generate the repo. CAUTION: this will cause all manifests in the bucket to
@@ -46,15 +52,17 @@ if $help || [[ $# -lt 1 ]]; then
 	exit 2
 fi
 
-if [[ -z "${AWS_ACCESS_KEY_ID:-}" || -z "${AWS_SECRET_ACCESS_KEY:-}" ]]; then
+if ! $dry_run && [[ -z "${AWS_ACCESS_KEY_ID:-}" || -z "${AWS_SECRET_ACCESS_KEY:-}" ]]; then
 	echo '$AWS_ACCESS_KEY_ID or $AWS_SECRET_ACCESS_KEY not set!' >&2
 	exit 2
 fi
 
 # a helper (print_or_export_manifest_cmd) called in the script invoked by Bob will write to this if set
 MANIFEST_CMD=$(mktemp -t "manifest.XXXXX")
+# a log for capturing the inner formula exit status
+bob_log=$(mktemp -t "bob-log.XXXXX")
 export MANIFEST_CMD
-trap 'rm -rf "$MANIFEST_CMD";' EXIT
+trap 'rm -f "$MANIFEST_CMD" "$bob_log";' EXIT
 
 # make sure we start cleanly
 rm -rf /app/.heroku/php
@@ -73,7 +81,18 @@ for var in "$@"; do
 	args+=("$var")
 done
 
-bob deploy "${args[@]}"
+bob $($dry_run && echo "build" || echo "deploy") "${args[@]}" |& tee "$bob_log" || {
+	orig_status=$?
+	# bob does not forward the exit status of the formula, but exits 1 and prints something like this to stderr:
+	# ERROR: Formula exited with return code 9.
+	inner_status=$(grep -Po "(?<=^ERROR: Formula exited with return code )\d(?=\.$)" "$bob_log") || {
+		# It wasn't that, so we return the original exit status, unless the archive existed, then we return status 5
+		grep -qE "^ERROR: Archive .+ already exists\.$" "$bob_log" && exit 5 || exit "$orig_status"
+	}
+	exit "$inner_status"
+}
+
+$dry_run && exit
 
 # invoke manifest upload
 echo ""
